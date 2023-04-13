0be7179f352ea3ed6e73ecc548e3860dae50a8e8
formountain~ <sgt0205@gmail.com~>
2023-04-14 02:36:07 +0900

    2023.04.1355

--- src/ch09/_04_ArrayList.java
+++ src/ch09/_04_ArrayList.java
@@ -19,5 +19,24 @@ public class _04_ArrayList {
         }
         list.set(0, "이수근");
         System.out.println(list.get(0));
+
+        System.out.println(list.indexOf("박명수")+1);
+        if(list.contains("박명수")){
+            System.out.println("수강신청 성공");
+        }else{
+            System.out.println("수강신청 실패");
+        }
+        list.clear();
+        if(list.isEmpty()){
+            System.out.println("비어있음"+list.size());
+        }else{
+            System.out.println("아직 남아있음");
+        }
+        list.add("유재석");
+        list.add("노홍철");
+        list.add("박명수");
+        list.add("하하");
+        list.add("정형돈");
+        list.add("정준하");
     }
 }
